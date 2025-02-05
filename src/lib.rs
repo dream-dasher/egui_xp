e4815fc0ee2adbc6a933c1dde0b6ed74b4625c70
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-02-05 16:57:27 -0500

    chore(cfg): result pub in wasm32

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,5 @@ pub mod support;
 pub use app::TemplateApp;
 #[cfg(not(target_arch = "wasm32"))]
 pub use support::activate_global_default_tracing_subscriber;
-#[cfg(not(target_arch = "wasm32"))]
+// #[cfg(not(target_arch = "wasm32"))]
 pub use support::{Error, Result};
