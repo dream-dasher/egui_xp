0a1d0932d7ee159625e34c0454be4b50ac26d061
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-02-28 13:11:25 -0500

    feat: option to write trace output to file

--- src/support/subscriber.rs
+++ src/support/subscriber.rs
@@ -4,11 +4,13 @@
 //! - Tracing is poorly documented and methods poorly named.  One can easily use, e.g., `::fmt()` instead of `::fmt` and be greeted with cryptic or even misdirecting errors.
 //!   - I have no solution for this.  *Just be careful!*  It is very easy to lose a lot of time chasing one's tail, on seemingly trivial configuration.
 
+use std::path::PathBuf;
+
 use bon::builder;
 use tracing::level_filters::LevelFilter;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_error::ErrorLayer;
-use tracing_subscriber::{fmt::format::FmtSpan, prelude::*};
+use tracing_subscriber::prelude::*;
 
 use crate::Result;
 
@@ -38,9 +40,25 @@ const DEFAULT_ERROR_LOGGING_LEVEL: LevelFilter = LevelFilter::WARN;
 pub fn activate_global_default_tracing_subscriber(
         env_default_level: Option<LevelFilter>,
         trace_error_level: Option<LevelFilter>,
+        file_to_write_to: Option<PathBuf>,
 ) -> Result<WorkerGuard> {
         let env_default_level = env_default_level.unwrap_or(DEFAULT_LOGGING_LEVEL);
         let trace_error_level = trace_error_level.unwrap_or(DEFAULT_ERROR_LOGGING_LEVEL);
+        let ((non_blocking_writer, trace_writer_guard), use_ansi) = match file_to_write_to {
+                None => (tracing_appender::non_blocking(std::io::stderr()), true),
+                Some(file_path) => {
+                        if let Some(parent) = file_path.parent() {
+                                std::fs::create_dir_all(parent)
+                                        .expect("parent path should have been created if not already present");
+                        }
+                        (
+                                tracing_appender::non_blocking(
+                                        std::fs::File::create(file_path).expect("log file should have been created"),
+                                ),
+                                false,
+                        )
+                }
+        };
 
         let envfilter_layer = tracing_subscriber::EnvFilter::builder()
                 .with_default_directive(env_default_level.into())
@@ -48,7 +66,6 @@ pub fn activate_global_default_tracing_subscriber(
 
         let error_layer = ErrorLayer::default().with_filter(trace_error_level);
 
-        let (non_blocking_writer, trace_writer_guard) = tracing_appender::non_blocking(std::io::stderr());
         let fmt_layer = tracing_subscriber::fmt::Layer::default()
                 // .compact()
                 // .pretty()
@@ -58,13 +75,13 @@ pub fn activate_global_default_tracing_subscriber(
                 .with_thread_names(true)
                 .with_file(true)
                 .with_line_number(true)
-                .with_span_events(FmtSpan::NONE)
-                .with_writer(non_blocking_writer)
-                .with_filter(envfilter_layer);
+                .with_ansi(use_ansi)
+                // .with_span_events(FmtSpan::FULL)
+                .with_writer(non_blocking_writer);
 
         let subscriber = tracing_subscriber::Registry::default()
                 .with(error_layer)
-                .with(fmt_layer);
+                .with(fmt_layer.with_filter(envfilter_layer));
 
         tracing::subscriber::set_global_default(subscriber)?;
         Ok(trace_writer_guard)
